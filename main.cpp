be4fef7c9b91f9ca76fdfbf6669747e93ab2d163
Akil0814 <akil040814@gmail.com>
2026-02-16 23:55:30 -0800

    2/16 p50

--- main.cpp
+++ main.cpp
@@ -76,36 +76,6 @@ void prepareVAO()
 
 }
 
-void prepare_buffer()
-{
-    float vertices[] =
-    {
-        -0.5f, -0.5f, 0.0f,
-         0.5f, -0.5f, 0.0f,
-         0.0f,  0.5f, 0.0f,
-
-         0.5f,  0.5f, 0.0f,
-         0.8f,  0.8f, 0.0f,
-         0.8f,  0.0f, 0.0f
-    };
-
-    GLuint VBO = 0;
-    glGenBuffers(1, &VBO);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-    VAO = 0;
-    glGenVertexArrays(1, &VAO);
-    glBindVertexArray(VAO);
-
-    //√Ë ˆŒª÷√–≈œ¢
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-
-    glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-
-    glBindVertexArray(0);
-}
 
 void prepare_shader()
 {
